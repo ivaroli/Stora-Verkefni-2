99387b27baff995b31d1bdb20fdd69b994779bf0
ivaroli <ivartheoli@gmail.com>
2018-05-02 13:12:19 +0000

    SignIn backend stuff

---
+++ Models/InputModels/UserInputModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookApp.Models
+{
+    public class UserInputModel
+    {
+        public string UserName{get; set;}
+        public string Password{get; set;}
+        public string Email{get; set;}
+    }
+}
