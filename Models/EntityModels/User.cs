4e496031a3b1401241658d3a57b6c337df50abf3
ivaroli <ivartheoli@gmail.com>
2018-05-03 11:38:44 +0000

    Dtatabase setup og css dót

--- Models/EntityModels/User.cs
+++ Models/EntityModels/User.cs
@@ -4,7 +4,7 @@ namespace BookApp.Models
 {
     public class User
     {
-        public int ID{get; set;}
+        public int Id{get; set;}
         public string UserName{get; set;}
         public string Email{get; set;}
         public string Type{get; set;}
