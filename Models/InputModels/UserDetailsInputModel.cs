f7240b0d433ac208fdd299b82a1f09ecc1ba3624
ivaroli <ivartheoli@gmail.com>
2018-05-04 21:21:41 +0000

    SignInDót

---
+++ Models/InputModels/UserDetailsInputModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Web;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace BookApp.Models
+{
+    public class UserDetailsInputModel
+    {
+        public string FirstName{get; set;}
+        public string LastName{get; set;}
+        public string Country{get; set;}
+        public string City{get; set;}
+        public string Address{get; set;}
+        //public HttpPostedFileBase Image{get; set;}
+    }
+}
