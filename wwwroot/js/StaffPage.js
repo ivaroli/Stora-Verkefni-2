478bee38b8a44229382e9bc048cd38f880acb4ac
ivaroli <ivartheoli@gmail.com>
2018-05-07 11:56:48 +0000

    Details

--- wwwroot/js/StaffPage.js
+++ wwwroot/js/StaffPage.js
@@ -1,7 +1,17 @@
-function insert()
+function getBooks(search)
 {
+    $.post("/Book/Search", {search}, function(result){
+    });
+}
+
+function getAuthors(search)
+{
+    $.post("/Authors/Search", {search}, function(result){
+    });
 }
 
 $(document).ready(function(e) {
-    $("")
+    $('#search').on('input',function(e){
+
+    });
 });
