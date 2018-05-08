4331d1af6b272503260936477826a5dc89cfde91
margretb17 <margretb17@ru.is>
2018-05-08 20:53:27 +0000

    öllu puslað saman aftur

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -21,24 +21,13 @@ namespace BookApp.Controllers
             
         }
 
-        [HttpGet]
+       [HttpGet]
         public IActionResult Details(int id)
         {
-            Console.WriteLine("ID = " + id );
-<<<<<<< HEAD
             var result = bookService.GetBooksByAuthorId(id);
-            Console.WriteLine("nr of books = " + result.Count);
             return View(result);
         }
-        [HttpGet]
-        public IActionResult Authors()
-        {
-            var authors = authorService.getAllAuthors();
-            return View(authors);
-=======
-            return View();
->>>>>>> 0b3f3ca46cfcab8f8f41f84aabd7717fed28389e
-        }
+
         [HttpGet]
         public IActionResult Authors()
         {
